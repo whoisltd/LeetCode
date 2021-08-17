9e2a8ed4165b50c31d850d1856d89114a4166986
Dat Le <tiendat.le129@gmail.com>
2021-08-17 16:08:52 +0000

    Sync LeetCode submission - Two Sum (python)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -5,11 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        lis = []
-        for i in range (0, len(nums)):
+        for i in range (len(nums)-1):
             for j in range (i+1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    lis.append(i)
-                    lis.append(j)
-            
-        return lis
+                    return [i, j]
