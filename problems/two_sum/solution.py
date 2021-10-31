1f4ba95e05fc299c5f24a807d4d1a7f7cbb0fe6d
Dat Le <tiendat.le129@gmail.com>
2021-10-31 16:44:46 +0000

    Sync LeetCode submission - Two Sum (python)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -5,7 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range (len(nums)-1):
-            for j in range (i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        hashmap = {}
+        for i in range(len(nums)):
+            a = target - nums[i]
+            if a in hashmap:
+                return [i, hashmap[a]]
+            hashmap[nums[i]] = i
+            
